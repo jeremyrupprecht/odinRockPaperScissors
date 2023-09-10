45c69220e8f4bc63caf8b1af4ca69176d9669f87
jeremyrupprecht <jdavid.rupprecht@gmail.com>
2023-09-09 18:06:30 -0600

    Clean up comments, (attempting) to bring them up to clean code standards

--- script.js
+++ script.js
@@ -1,46 +1,22 @@
 /* 
-
-Problem solving:
-
-1. Understand the problem/write it in english
-
-    -Implement the game "Rock Paper Scissors", where 2 players,
-    (a player and a computer opponent who makes random moves) pick
-    between 3 choices of hand signal (rock, paper and scissors). 
-    Rock beats scissors, scissors beats paper, and paper beats rock.
-    The player is prompted to make their choice of signal, then 
-    this choice is computed against the choice of the computer.
-    Finally the resulting winner is displayed to the screen
-
-
-2. Ask questions about the problem (UI? Input(s)? Output(s)? 
-   How to get from Input to Output?)
-
-   -UI? -->         Text-only, prompt based inputs
-   -Input(s) -->    The player is prompted uing text prompts
-   -Output(s) -->   A string showing the winner of the round
-   -How to? -->     ...
-
-
-3. Psuedocode/subproblems is provided for each function
-
+Author: Jeremy Rupprecht
+Date: 2023-09-09
+Description: 
+    This script implements a trivial console only based version of the game Rock
+    Paper Scissors. The game is played between a person and a computer where
+    both pick between 3 choices of hand signal (rock, paper or scissors) at 
+    the same time (each time counts as a single round). Rock beats scissors, 
+    scissors beats paper, and paper beats rock. The player is prompted to 
+    make their choice of signal, then this choice is computed against the choice
+    of the computer. After 5 rounds, the resulting winner is displayed to the screen
 */
 
 /*
-Pseudocode: This function generates the computers signal choice
-    1. Generate a random number between 1-3 (or 0-2)
-    2. Return the string "Rock" or "Paper" or "Scissors"
-       based on the random number
-
+This function generates the computers random signal choice
 */
-
-function getComputerChoice() {
-
-    // 1.
+function getRandomComputerChoice() {
 
     let random = Math.random() * 3;
-
-    // 2. 
 
     if (random < 1) {
         return "Rock"
@@ -53,26 +29,16 @@ function getComputerChoice() {
 
 }
 
-/*
-Pseudocode: This function computes the players signal choice 
-            against the computers signal choice and returns 
-            the resulting winner
-    1. Compare the player choice agaisnt the computer choice
-    2. Return the winning choice based on the rules of rock paper 
-       scissors
-           -if there is a tie, return that result
-
-    -note: Rock beats scissors, scissors beats paper, and paper beats rock
-*/
 function playRound(playerSelection, computerSelection) {
 
+    // Account for case sensitivity
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
-    //1. and //2 combined
     if (playerSelection === computerSelection) {
         return `Tie! ${playerSelection} ties ${computerSelection}, play again!`
     } else {
+        // Rock beats scissors, scissors beats paper, and paper beats rock
         if ((playerSelection === "rock" && computerSelection === "scissors")
             || (playerSelection === "scissors" && computerSelection === "paper")
             || (playerSelection === "paper" && computerSelection === "rock")) {
@@ -83,21 +49,14 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-/*
-Pseudocode: This function retrives input from the player
-
-*/
-
 function getPlayerChoice() {
     let choice = prompt("Enter One of Three Choices (Rock, Paper, or Scissors)");
     return choice;
 }
 
 /*
-Pseudocode: ...
-
+This function checks the player signal choice for null input, case sensitivty and spelling
 */
-
 function checkPlayerChoiceValidity(choice) {
 
     if (choice === null) {
@@ -122,49 +81,34 @@ function checkPlayerChoiceValidity(choice) {
 }
 
 /*
-Pseudocode: This function plays 5 rounds of rock paper scissors
-            tracking the score and reporting a winner or loser 
-            at the end of the 5 rounds
-    1. Loop 5 times
-    2. Retrive player response (and check it for validity, ask the 
-        player again if they enter in an invalid choice)
-    3. Play the game
-    4. Retrieve the result of each round, if it's a tie, 
-       replay the round without taking up a round of the 5
-    5. Update the score each time
-    6. At the end of the 5 loops, display the winner and loser 
-       (or a tie?)
+This function plays 5 rounds of rock paper scissors. Round ties and invalid
+player inputs do not count as full rounds and are replayed
 */
 
-function game() {
+function playGame() {
 
-    // 1. 
-    let gameCounter = 0;
+    let roundCounter = 0;
     let keepGameGoing = true;
     let playerScore = 0;
     let computerScore = 0;
 
     while (keepGameGoing) {
 
-        // 2. If player choice is invalid, prompt the player again
-
         let playerChoice = getPlayerChoice() 
 
+        //If player choice is invalid, prompt the player again without incrementing the round counter
         if (!(checkPlayerChoiceValidity(playerChoice))) {
             console.log("Your choice is of the incorrect form or null, please choose either 'Rock' or 'Paper' or 'Scissors'")
             continue;
         }
 
-        // 3. 
-
-        let computerChoice = getComputerChoice();
+        let computerChoice = getRandomComputerChoice();
         console.log(`Your choice is ${playerChoice}`)
         console.log(`The computer's choice is ${computerChoice}`)
 
         let result = playRound(playerChoice, computerChoice);
 
-        //4. and 5.
-
+        //If the round is a tie, retry the round without incrementing the round counter
         console.log(result)
         if (result.includes("Tie")) {
            continue;
@@ -176,11 +120,9 @@ function game() {
 
         console.log(`The Current Scores are: Player: ${playerScore}, Computer: ${computerScore}`)
 
-        // 6. 
+        roundCounter = roundCounter + 1;
 
-        gameCounter = gameCounter + 1;
-
-        if (gameCounter >= 5) {
+        if (roundCounter >= 5) {
             keepGameGoing = false;
 
             if (playerScore > computerScore) {
@@ -196,25 +138,4 @@ function game() {
     
 }
 
-// Player vs Computer logic testing
-/*
-console.log(playRound("Rock", "Rock"))
-console.log(playRound("Rock", "Paper"))
-console.log(playRound("Rock", "Scissors"))
-console.log("------------------------------")
-console.log(playRound("Paper", "Rock"))
-console.log(playRound("Paper", "Paper"))
-console.log(playRound("Paper", "Scissors"))
-console.log("------------------------------")
-console.log(playRound("Scissors", "Rock"))
-console.log(playRound("Scissors", "Paper"))
-console.log(playRound("Scissors", "Scissors"))
-*/
-
-/*
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-*/
-
-game();
+playGame();
